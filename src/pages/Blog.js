ac7d19e48453a6d832470d826ce3dc6b40acaca5
EwuraAnnie <ewuranhyira03@gmail.com>
2022-04-04 14:09:36 +0000

    react-component-8

---
+++ src/pages/Blog.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Blog = () => {
+  return (
+    <section>
+        <h1>Welcome To The Blog Page</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid sequi optio tempora ut libero iure distinctio fuga quaerat soluta harum, veniam perferendis corporis voluptas modi error ducimus? Reiciendis, quo eaque!
+        Aperiam, optio! Obcaecati molestias dolorum voluptates fugiat, voluptas ipsa! Harum fugiat dolor veritatis mollitia distinctio ratione voluptates sequi quasi, totam, libero voluptatum quidem sed cupiditate id labore pariatur? Amet, exercitationem!
+        Perspiciatis, aperiam. Saepe cumque, beatae maiores eum iste quos atque eius odit aspernatur doloribus pariatur nulla quo obcaecati minus ratione expedita dolorum sunt ipsam, quia tempore veniam commodi! Autem, quas.
+        </p>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid sequi optio tempora ut libero iure distinctio fuga quaerat soluta harum, veniam perferendis corporis voluptas modi error ducimus? Reiciendis, quo eaque!
+        Aperiam, optio! Obcaecati molestias dolorum voluptates fugiat, voluptas ipsa! Harum fugiat dolor veritatis mollitia distinctio ratione voluptates sequi quasi, totam, libero voluptatum quidem sed cupiditate id labore pariatur? Amet, exercitationem!
+        Perspiciatis, aperiam. Saepe cumque, beatae maiores eum iste quos atque eius odit aspernatur doloribus pariatur nulla quo obcaecati minus ratione expedita dolorum sunt ipsam, quia tempore veniam commodi! Autem, quas.
+        </p>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid sequi optio tempora ut libero iure distinctio fuga quaerat soluta harum, veniam perferendis corporis voluptas modi error ducimus? Reiciendis, quo eaque!
+        Aperiam, optio! Obcaecati molestias dolorum voluptates fugiat, voluptas ipsa! Harum fugiat dolor veritatis mollitia distinctio ratione voluptates sequi quasi, totam, libero voluptatum quidem sed cupiditate id labore pariatur? Amet, exercitationem!
+        Perspiciatis, aperiam. Saepe cumque, beatae maiores eum iste quos atque eius odit aspernatur doloribus pariatur nulla quo obcaecati minus ratione expedita dolorum sunt ipsam, quia tempore veniam commodi! Autem, quas.
+        </p>
+    </section>
+  )
+}
+
+export default Blog;
